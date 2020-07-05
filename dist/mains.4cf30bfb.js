380ed17949032d9412141f9d69e3e55038a4de1d
Laura-deVere <lauradevh@gmail.com>
2020-07-04 21:17:10 -0400

    added list by author

--- dist/mains.4cf30bfb.js
+++ dist/mains.4cf30bfb.js
@@ -120,18 +120,19 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"mains.js":[function(require,module,exports) {
 const generator = (() => {
   const htmlEls = {
-    quote: document.getElementById('quote'),
-    author: document.getElementById('author')
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+    reload: document.getElementById("reload")
   };
 
   const getData = async () => {
     const url = `https://type.fit/api/quotes`;
     const proxyurl = `https://cors-anywhere.herokuapp.com/`;
     return fetch(proxyurl + url, data = {}, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application / json, text / plain, */*'
+        "Content-Type": "application/json",
+        Accept: "application / json, text / plain, */*"
       },
       body: JSON.stringify(data)
     }).then(response => {
@@ -141,19 +142,38 @@ const generator = (() => {
     });
   };
 
-  const displayQuote = async () => {
-    const data = await getData();
-    console.log(data);
-    let max = data.length;
-    let min = 0; // Math.random returns a floating point between 0 & 1
-
-    let random = data[Math.round(Math.random() * (max - min) + min)];
-    console.log(random);
-    htmlEls.quote.textContent = random.text;
-    htmlEls.author.textContent = random.author;
+  const randomNum = (max, min) => {
+    // Math.random returns a floating point between 0 & 1
+    let num = Math.round(Math.random() * (max - min) + min);
+    return num;
   };
 
-  displayQuote();
+  const searchAuthor = (name, data) => {
+    // let regex = new RegExp(name, "i");
+    let quotes = [];
+    data.forEach(quote => {
+      if (quote.author !== null && quote.author.indexOf(name) !== -1) {
+        quotes.push(quote);
+      }
+    });
+    return displayQuote(quotes[randomNum(quotes.length - 1, 0)], quotes);
+  };
+
+  const displayQuote = (quote, data) => {
+    htmlEls.quote.textContent = quote.text;
+    htmlEls.author.textContent = quote.author ? quote.author : "Unknown";
+    htmlEls.author.addEventListener("click", () => searchAuthor(quote.author, data));
+  };
+
+  const randomQuote = async () => {
+    const data = await getData();
+    let max = data.length;
+    let random = data[randomNum(max, 0)];
+    displayQuote(random, data);
+  };
+
+  htmlEls.reload.addEventListener("click", randomQuote);
+  randomQuote();
 })();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -183,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62484" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50700" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
